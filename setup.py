0e85623c8828bb96edce362b2f5c81e815cf8c47
Xolisa Mzini <xolisa.mzini@gmail.com>
2019-03-19 13:07:06 +0200

    changed setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='package',
+    name='mypackage',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='basic sorting and recursion functions in python',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/xolisa003/package',
+    url='https://github.com/xolisa003/palgorithms',
     author='Xolisa Mzini',
     author_email='xolisa.mzini@gmail.com'
 )
